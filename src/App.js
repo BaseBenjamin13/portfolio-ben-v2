b3b6849f5a9fea3af696e909791463f402597b65
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-12-29 21:24:27 -0500

    cleaning up start page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import About from './pages/About';
-import StartPage from './pages/StartPage';
+// import StartPage from './pages/StartPage';
 import Home from './pages/Home';
 
 
