4d740c236e7abb7220db94b6c7a9024766ba712e
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-02 20:43:31 -0500

    adding about me content to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header'; 
 import ProjectList from '../components/projects/ProjectList';
 import GitHistory from '../components/GitHistory';
-
+// import About from './About';
+import BrandStatement from '../components/aboutme/BrandStatement';
+import LangsAndDatabases from '../components/aboutme/LangsAndDatabases';
+import FrameWorks from '../components/aboutme/FrameWorks';
+import Techs from '../components/aboutme/Techs';
 
 function Home() {
   return (
@@ -18,6 +22,15 @@ function Home() {
 
         <h1 className="my-projects-header">My Projects</h1>
         <ProjectList />
+     
+        <BrandStatement />
+
+        <LangsAndDatabases />
+
+        <FrameWorks />
+
+        <Techs />
+
     </div>
   )
 }
