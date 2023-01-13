ba2bed352a4c493690176b59f4f7ad2531369b40
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-12 20:59:33 -0500

    added description meta tag

--- src/components/aboutme/BrandStatement.js
+++ src/components/aboutme/BrandStatement.js
@@ -6,7 +6,7 @@ function BrandStatement() {
     <div className="brand-statement-container">
         <h1 className="about-me-title">About Me</h1>
         <p className="brand-statement-body">
-        I am a full-stack software engineer who loves to write and fix code to find
+        I Benjamin Morgiewicz, am a full-stack software engineer who loves to write and fix code to find
         a solution to any problem. I use my problem-solving brain and hard work
         experience to make life easier and more fun with intelligent code.
         </p>
