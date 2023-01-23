e127c5a32005b34a6e8294f730a0c8df5a6bf429
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-22 19:39:02 -0500

    made git history mobile responsive

--- src/components/GitHistory.js
+++ src/components/GitHistory.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Heading, Stack, useColorModeValue as uCMV } from '@chakra-ui/react';
 import GitHubCalendar from 'react-github-calendar';
-import ReactTooltip from 'react-tooltip';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 function GitHistory() {
-  return (
+
+    const matches = useMediaQuery('(max-width:600px)');
+
+    return (
       <div className="git-history-container">
 
     <Stack paddingTop={20}>
@@ -14,23 +18,17 @@ function GitHistory() {
             marginY={5}>
             My GitHub History
         </Heading>
-        {/* <GitHubCalendar username="basebenjamin13"/> */}
+        
+        <div class="git-history">
+
         <GitHubCalendar
-            fontSize={25}
+            fontSize={matches ? 15 : 25}
             blockSize={15}
             blockRadius={5}
             color={"#38A169"}
-            
-            // theme={{
-            //         level0: uCMV("rgb(200,200,200)","rgb(44,44,44)"),
-            //         level1: uCMV('goldenrod','rgb(107,91,47)'),
-            //         level2: uCMV('rgb(176,145,66)','rgb(137,115,31)'),
-            //         level3: uCMV('rgb(147,125,41)','rgb(166,135,56)'),
-            //         level4: uCMV('rgb(117,101,57)','goldenrod') 
-            //     }}
-                username="basebenjamin13">
-            {/* <ReactTooltip html /> */}
+            username="basebenjamin13">
         </GitHubCalendar>
+        </div>
     </Stack>
     </div>
   )
