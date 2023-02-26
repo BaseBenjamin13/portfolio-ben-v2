eb047da75dab8a8e80caa954b1c171c943c7a999
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-02-26 10:27:27 -0500

    added links to framework icons

--- src/components/aboutme/FrameWorks.js
+++ src/components/aboutme/FrameWorks.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 
+
 function FrameWorks() {
 
     const [frameIcons, setFrameIcons] = useState([
         {
             title: "React",
             svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-            alt: "react icon"
+            alt: "react icon",
+            link: "https://reactjs.org"
         },
         {
             title: "Django",
             svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
-            alt: "django icon"
+            alt: "django icon",
+            link: "https://www.djangoproject.com"
         },
         {
             title: "Node.js",
             svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-            alt: "node js icon"
+            alt: "node js icon",
+            link: "https://nodejs.org/en/"
         },
         {
             title: "Express",
             svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
-            alt: "express icon"
+            alt: "express icon",
+            link: "https://expressjs.com"
         },
         {
             title: "Bootstrap",
             svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
-            alt: "boot strap icon"
+            alt: "boot strap icon",
+            link: "https://getbootstrap.com"
         }
     ])
 
@@ -37,10 +43,12 @@ function FrameWorks() {
                 {
                     frameIcons.map((icon, index) => {
                         return(
+                            <a href={icon.link} target="_blank" className="remove-a-style">
                             <div className="icon" key={index}>
                                 <img src={icon.svg} className="frame-icon" alt={icon.alt} />
                                 <h2>{icon.title}</h2>
                             </div>
+                            </a>
                         )
                     })
                 }
